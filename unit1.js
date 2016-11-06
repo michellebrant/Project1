0c0e04a000e267a0e7550633cc8c0b832f537056
Michelle Brant <michellebrant>
2016-11-06 15:07:46 -0500

    add with extra css

--- unit1.js
+++ unit1.js
@@ -2,17 +2,11 @@ console.log('hey!')
     // creates opening screen
 body = $('body');
 message = $('#message')
-message.append('<p class ="first">Welcome to Inside Out! Read the rules before you play!</p>');
+insideoutlogo = $('<img src="insideoutlogo.jpg" class="logo">');
+body.append(insideoutlogo)
+message.append('<p class="welcome">Welcome to Inside Out : Save Riley\'s Memories Game</p>');
 message.append('<button class="rules">Rules</button>');
 start = $('<button class="start">Start the Game!</button>')
-body.append('<div class="joy-1" id="JOY"></div>');
-joy = $('.joy-1')
-body.append('<div class="disgust-1" id="DISGUST"></div>');
-disgust = $('.disgust-1')
-body.append('<div class="sad-1" id="SAD"></div>');
-sad = $('.sad-1')
-body.append('<div class="anger-1" id="ANGER" ></div>');
-anger = $('.anger-1')
 
 function Anger() {
     var counter = 0
@@ -72,12 +66,26 @@ function Joy() {
 $('.rules').on("click", function() {
     $('.first').remove();
     $('.rules').remove();
-    message.append('<p class="message_text">Help Riley save her memories! Collect points by clicking on memories before they fall into the memory dump. Purple memory orbs are regular memories worth 10 points. The yellow memories are core memories- loose more than three, and game over!</p>')
+    $(".welcome").remove();
+    message.append('<p>In this game based on Pixar\'s hit film Inside Out (2015), Riley\'s memories are in danger of being lost forever in the Memory Dump. Help Riley save her memories or risk loosing her personality islands forever.</p>');
+    message.append('<p>Once the game begins, Riley\'s memories will fall from the top of the screen. Purple memories signal regular memories. In order to save these memories click the memory orb, and gain 10 points. These memories will not cause a total removal of a personality island, but will dock 20 points from your score. Fall below a score of 0 and you loose!</p>');
+    message.append('<p>In addition to regular memories, golden core memory orbs will be falling sporadically. These are the most important memories Riley has, and make up her personality. In order to save these, you must double click the orb (you will gain 20 points). Miss one, and Riley looses a personality island and you automatically loose the game.</p>')
     message.append(start)
 })
 
 start.on("click", function() {
-   startLevelOne();
+    body.append('<div class="joy-1" id="JOY"></div>');
+    joy = $('.joy-1')
+    body.append('<div class="disgust-1" id="DISGUST"></div>');
+    disgust = $('.disgust-1')
+    body.append('<div class="sad-1" id="SAD"></div>');
+    sad = $('.sad-1');
+    body.append('<div class="anger-1" id="ANGER" ></div>');
+    anger = $('.anger-1');
+    body.append('<div class="score"></div>');
+    $('.score').append('<p class = "points">You have 0 points and have saved 0 memories! </p>');
+    startLevelOne();
+    insideoutlogo.remove();
 
 })
 
@@ -174,111 +182,105 @@ function checkToLoose() {
 
     }
 
-    }
-;
+};
 looseID1 = setInterval(function() {
     checkToLoose();
 }, 100)
 
-function startLevelOne(){
-  createMems();
-  createCoreMems();
-   $('#message').remove();
+function startLevelOne() {
+    createMems();
+    createCoreMems();
+    $('#message').remove();
     start.remove();
-    body.append('<div class="score"></div>')
-    $('.score').append('<p class = "points">You have 0 points and have saved 0 memories! </p>');
+
 }
 
-function checkToWinLevelOne(){
+function checkToWinLevelOne() {
     scoreboard = $('.points')
     splitBoard = scoreboard.text().split(' ');
     points = parseInt(splitBoard[2])
-    if (points >=100 ){
-      $('.corememory').remove();
-      $('.regmemory').remove();
-      clearInterval(coreIntervalId);
-      clearInterval(memoryIntervalID);
-      body.append(message);
-      $('.message_text').text('You beat level 1! Level 2 starts in 3 seconds');
-      Anger();
-      Sad();
-      Joy();
-      Disgust();
-      clearInterval(winID1);
-      setTimeout (startLevelTwo, 3000);
-      }
+    if (points >= 100) {
+        $('.corememory').remove();
+        $('.regmemory').remove();
+        clearInterval(coreIntervalId);
+        clearInterval(memoryIntervalID);
+        body.append('<div class="levelmessage"></div');
+        $('.levelmessage').append('<p class="levelmessage">You beat level 1! Level 2 starts in 3 seconds</p>');
+        Anger();
+        Sad();
+        Joy();
+        Disgust();
+        clearInterval(winID1);
+        setTimeout(startLevelTwo, 3000);
+    }
 }
 winID1 = setInterval(function() {
     checkToWinLevelOne();
 }, 100)
 
-function startLevelTwo(){
+function startLevelTwo() {
     $('#message').remove();
-    start.remove();
+    $('.levelmessage').remove();
     createMems();
     scoreboard.text("You have 0 points and have saved " + newMemories + " memories!");
     createCoreMems();
     winID2 = setInterval(function() {
-    checkToWinLevelTwo();
-}, 100)
+        checkToWinLevelTwo();
+    }, 100)
 
 }
-function checkToWinLevelTwo(){
+
+function checkToWinLevelTwo() {
     scoreboard = $('.points')
     splitBoard = scoreboard.text().split(' ');
     points = parseInt(splitBoard[2])
-    if (points >=100 ){
-      $('.corememory').remove();
-      $('.regmemory').remove();
-      clearInterval(coreIntervalId);
-      clearInterval(memoryIntervalID);
-      body.append(message);
-      $('.message_text').text('You beat level 2! Level 3 starts in 3 seconds');
-       setTimeout (startLevelThree, 3000);
-      Anger();
-      Sad();
-      Joy();
-      Disgust();
-      clearInterval(winID2);
-      }
+    if (points >= 100) {
+        $('.corememory').remove();
+        $('.regmemory').remove();
+        clearInterval(coreIntervalId);
+        clearInterval(memoryIntervalID);
+        body.append('<div class="levelmessage"></div');
+        $('.levelmessage').append('<p class="levelmessage">You beat level 2! Level 3 starts in 3 seconds</p>');
+        setTimeout(startLevelThree, 3000);
+        Anger();
+        Sad();
+        Joy();
+        Disgust();
+        clearInterval(winID2);
+    }
 }
 
-function startLevelThree(){
+function startLevelThree() {
     $('#message').remove();
-    start.remove();
+    $('.levelmessage').remove();
     createMems();
     scoreboard.text("You have 0 points and have saved " + newMemories + " memories!");
     createCoreMems();
     winID3 = setInterval(function() {
-    checkToWinLevelThree();
-}, 100)
+        checkToWinLevelThree();
+    }, 100)
 
 }
-function checkToWinLevelThree(){
+
+function checkToWinLevelThree() {
     scoreboard = $('.points')
     splitBoard = scoreboard.text().split(' ');
     points = parseInt(splitBoard[2])
-    if (points >=100 ){
-      $('.corememory').remove();
-      $('.regmemory').remove();
-      clearInterval(coreIntervalId);
-      clearInterval(memoryIntervalID);
-      body.append('<div class="winnermessage"></div>');
-      $('.winnermessage').append('<p>Congrats! You helped Riley save all her core memories!</p>')
-      $('.winnermessage').append('<p>To play again, click below.</p>')
-      $('.winnermessage').append('<button>Play Again!</button>')
-      body.append('<img src="core.png" class="winner">');
-      scoreboard.remove();
-      $('.score').remove();
-      anger.remove();
-      sad.remove();
-      joy.remove();
-      disgust.remove();
-      clearInterval(winID3);
-      }
+    if (points >= 100) {
+        $('.corememory').remove();
+        $('.regmemory').remove();
+        clearInterval(coreIntervalId);
+        clearInterval(memoryIntervalID);
+        body.append('<div class="winnermessage"></div>');
+        $('.winnermessage').append('<p class="winnermessage">Congrats, you helped Riley save all her memories! Click below to play again!</p>');
+        // $('.winnermessage').append('<button>Play Again!</button>')
+        body.append('<img src="core.png" class="winner">');
+        scoreboard.remove();
+        $('.score').remove();
+        anger.remove();
+        sad.remove();
+        joy.remove();
+        disgust.remove();
+        clearInterval(winID3);
+    }
 }
-
-
-
-
-
